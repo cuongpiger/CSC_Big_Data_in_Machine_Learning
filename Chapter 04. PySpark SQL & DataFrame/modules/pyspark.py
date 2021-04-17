8cc8a9f4b61f8d29ae13c58b5b6a41ee7b701cef
Manh Cuong <cuongpigerr@gmail.com>
2021-04-17 23:56:23 +0700

    .

--- Chapter 04. PySpark SQL & DataFrame/modules/pyspark.py
+++ Chapter 04. PySpark SQL & DataFrame/modules/pyspark.py
@@ -2,11 +2,10 @@ import pyspark
 import numpy as np
 from pyspark import SparkContext
 from pyspark.sql import SparkSession, Row
+from pyspark.sql.context import SQLContext
 from time import time
 
 from typing import List, Callable, List
-
-from pyspark.sql.context import SQLContext
 
 
 def measureRun(function):
@@ -17,6 +16,43 @@ def measureRun(function):
     return '{} mili-seconds'.format(round((end - start)*1000, 7))
 
 
+class CPySpark:
+    def __init__(self, app_name: str = None, session=False, sql=False):
+        self.context = SparkContext(master='local', appName=app_name)
+        self.session = None
+        self.sql = None
+
+        if session:
+            self.session = SparkSession(self.context)
+
+        if sql:
+            self.sql = SQLContext(self.context)
+
+    def rdd(self, data: np.ndarray = None, file: str = None, min_partitions: int = None, cache=False) -> 'CRDD(pyspark.RDD)':
+        res: pyspark.RDD = None
+
+        if data is not None:
+            res = self.context.parallelize(data)
+
+        if file is not None:
+            res = self.context.textFile(file, minPartitions=min_partitions)
+
+        if cache:
+            res = res.cache()
+
+        return CRDD(res)
+
+    def dataframe(self, crdd: 'CRDD'):
+        return CSparkFrame(self.sql.createDataFrame(crdd.rdd))
+
+    def read(self, file_name: str, option='csv'):
+        if option == 'csv':
+            return CSparkFrame(self.session.read.csv(file_name, inferSchema=True, header=True))
+
+        if option == 'json':
+            return CSparkFrame(self.session.read.json(file_name))
+
+
 class CRDD:
     def __init__(self, rdd: pyspark.RDD):
         self.rdd = rdd
@@ -24,7 +60,10 @@ class CRDD:
     def __len__(self):
         # return len(self.rdd.collect())
         return self.rdd.count()
-    
+
+    def __call__(self) -> pyspark.RDD:
+        return self.rdd
+
     def getFirst(self) -> list:
         """ Lấy ra record đầu tiên từ `self.rdd`
 
@@ -43,13 +82,13 @@ class CRDD:
             (List[str]): [description]
         """
         return self.rdd.take(amount)
-    
+
     def filter(self, def_: Callable):
         return CRDD(self.rdd.filter(def_))
-    
+
     def map(self, def_: Callable):
         return CRDD(self.rdd.map(def_))
-    
+
     def toNdarray(self):
         """ Chuyển đổi `self.rdd` thành ndarray
 
@@ -57,7 +96,7 @@ class CRDD:
             (np.ndarray):
         """
         return np.array(self.rdd.collect())
-    
+
     def getSamples(self, fraction: float, duplicate: bool = False, seed: int = 69) -> 'CRDD':
         """ Lấy mẫu từ một rdd nào đó
 
@@ -70,7 +109,7 @@ class CRDD:
             [type]: [description]
         """
         return CRDD(self.rdd.sample(duplicate, fraction, seed))
-    
+
     def subtract(self, other: 'CRDD'):
         """ Lấy ra phần (self.rdd - (self.rdd [intersection] other.rdd))
 
@@ -81,7 +120,7 @@ class CRDD:
             (CRDD): CRDD mới
         """
         return CRDD(self.rdd.subtract(other.rdd))
-    
+
     def unique(self):
         """ Lấy ra các record duy nhất
 
@@ -89,7 +128,7 @@ class CRDD:
             CRDD: 
         """
         return CRDD(self.rdd.distinct())
-    
+
     def merge(self, other: 'CRDD', how: str = 'cross'):
         """ Hàm kết hợp hai rdd
 
@@ -103,7 +142,7 @@ class CRDD:
         """
         if how == 'cross':
             return CRDD(self.rdd.cartesian(other.rdd))
-        
+
     def getAmountPartitions(self) -> int:
         """ Lấy ra số lượng partition mà rdd này tạo ra
 
@@ -111,7 +150,7 @@ class CRDD:
             (int): số lượng partition của self.rdd
         """
         return self.rdd.getNumPartitions()
-    
+
     def save(self, path: str, how='text'):
         """ Dùng để lưu rdd ra các dạng thư mục chứa các file
 
@@ -123,76 +162,47 @@ class CRDD:
         try:
             if how == 'text':
                 self.rdd.saveAsTextFile(path)
-        
+
             print('Success.')
         except:
             print('Error!')
-            
+
     def sort(self, by='key', ascending=True):
         if by == 'key':
             return CRDD(self.rdd.sortByKey(ascending=ascending))
-        
+
     def count(self, by='key'):
         if by == 'key':
             return self.rdd.countByKey()
-        
+
     def group(self, by='key'):
         if by == 'key':
             return CRDD(self.rdd.groupByKey())
-        
+
     def reduce(self, def_: Callable, by='key'):
         if by == 'key':
             return CRDD(self.rdd.reduceByKey(def_))
 
-class CPySpark:
-    def __init__(self, app_name: str = None, session=False, sql=False):
-        self.context = SparkContext(master='local', appName=app_name)
-        self.session = None
-        self.sql = None
-        
-        if session:
-            self.session = SparkSession(self.context)
-            
-        if sql:
-            self.sql = SQLContext(self.context)
-
-    def rdd(self, data: np.ndarray = None, file: str = None, min_partitions: int = None, cache=False) -> CRDD(pyspark.RDD):
-        res: pyspark.RDD = None
-
-        if data is not None:
-            res = self.context.parallelize(data)
-
-        if file is not None:
-            res = self.context.textFile(file, minPartitions=min_partitions)
-
-        if cache:
-            res = res.cache()
-
-        return CRDD(res)
-    
-    def dataframe(self, crdd: 'CRDD'):
-        return CSparkFrame(self.sql.createDataFrame(crdd.rdd))
-    
-    def read(self, file_name: str, option='csv'):
-        if option == 'csv':
-            return CSparkFrame(self.session.read.csv(file_name, inferSchema=True, header=True))
-        
-        if option == 'json':
-            return CSparkFrame(self.session.read.json(file_name))
-            
 
 class CSparkFrame:
     def __init__(self, dataframe: pyspark.sql.dataframe):
         self.dataframe = dataframe
-        
+
     def __len__(self):
         return self.dataframe.count()
-    
+
     def __call__(self):
         return self.dataframe
-        
-    def getHead(self, amount: int = 5):
+
+    def getHead(self, amount: int = None):
+        # get all
+        if amount is None:
+            amount = len(self)
+
         return self.dataframe.show(amount)
     
+    def describe(self):
+        return self.dataframe.describe().toPandas()
+
     def schema(self):
         return self.dataframe.printSchema()
