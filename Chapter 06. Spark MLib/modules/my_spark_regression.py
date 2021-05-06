a98f7d8042e140db31a5a402afc6908b905d0121
Manh Cuong <cuongpigerr@gmail.com>
2021-05-06 21:22:03 +0700

    .

--- Chapter 06. Spark MLib/modules/my_spark_regression.py
+++ Chapter 06. Spark MLib/modules/my_spark_regression.py
@@ -1,41 +1,61 @@
-from pyspark.ml.linalg import Vectors
+import pyspark
+
 from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.regression import LinearRegression
-from pyspark.ml.classification import LogisticRegression
-from pyspark.sql.functions import corr
-from pyspark.ml.feature import StringIndexer
-from pyspark.ml.feature import OneHotEncoder
-from pyspark.ml import Pipeline
-from pyspark.ml.evaluation import BinaryClassificationEvaluator
-
-import pyspark
 from typing import List
 
 
-class MySparkRegression:
-    def __init__(self, data: pyspark.RDD):
-        self.data = data
-        self.final_data = None
-        self.train = None
-        self.test = None
-        self.input: List[str] = None
-        self.output: str = None
-        self.model = None
+from modules.my_drawer import MyDrawer
 
-    def transform(self, input: List[str], output: str, input_label: str = None):
-        if input_label is None:
-            input_label = 'features'
-            
-        self.input = input
-        self.input_label = input_label
-        self.output = output
+
+class MySparkRegression:
+    def __init__(self, pData: pyspark.RDD, pPredictorFeatures: List[str], pTargetFeature: str):
+        self.data: pyspark.RDD = pData  # raw data
+        # features from `pData` used to training model
+        self.predictor_features: List[str] = pPredictorFeatures
+        self.target_feature: str = pTargetFeature
+        self.df_data: pyspark.sql.DataFrame = None
+        self.train_data: pyspark.sql.DataFrame = None
+        self.test_data: pyspark.sql.DataFrame = None
+
+    def prepareData(self):
+        """ Phương thức này dùng để chuẩn bị dữ liệu cho model, cụ thể nó sẽ chuyển tất cả các thuộc tính
+                trong `self.predictor_features` thành một vector với số chiều la length của 
+                `self.predictor_features`, cuối cùng đặt tên cho vector này là `features`
+        """
+        vector_assembler = VectorAssembler(
+            inputCols=self.predictor_features, outputCol='features')
+        self.df_data = vector_assembler.transform(
+            self.data).select('features', self.target_feature)
+
+    def trainTestSplit(self, pTrainSize: float = .75):
+        """ Tách dữ liệu ban đầu thành train data và test data với size của train data dc quy định bởi
+                tham số `pTrainSize`
+        Args:
+            pTrainSize (float, optional): Kích thước của `self.train_data`
+        """
+        if self.df_data is None:
+            print("🚫 Please call method prepareData() before calling this method!")
+            return
+
+        self.train_data, self.test_data = self.df_data.randomSplit(
+            (pTrainSize, 1 - pTrainSize))
+
+
+    def describeTarget(self, visual=False):
+        train_target_col = self.target_feature + " (train)"
+        test_target_col = self.target_feature + " (test)"
         
-        assembler = VectorAssembler(inputCols=self.input, outputCols=self.input_label)
-        self.final_data = assembler.transform(self.data).select(self.input_label, self.output)
+        train_describe: pyspark.sql.DataFrame = self.train_data.select(self.target_feature).describe().withColumnRenamed(self.target_feature, train_target_col)
+        test_describe: pyspark.sql.DataFrame = self.test_data.select(self.target_feature).describe().withColumnRenamed(self.target_feature, test_target_col)
+        entire_describe: pyspark.sql.DataFrame = train_describe.join(test_describe, on="summary")
         
-    def prepareData(self, train_size: float = .75):
-        self.train, self.test = self.final_data.randomSplit((train_size, 1 - train_size))
+        if visual:
+            describeTargetVisual()
         
-    def buildModel(self):
-        lr = LinearRegression(featuresCol=self.input_label, labelCol=self.output, predictionCol='predict_' + self.output)
-        self.model = lr.fit(self.data)
+        return train_describe.join(test_describe, on="summary")
+    
+    
+    
+def describeTargetVisual(pDesribeDataFrame: pyspark.sql.DataFrame):
+    y_scale = pDesribeDataFrame.select('summary')
