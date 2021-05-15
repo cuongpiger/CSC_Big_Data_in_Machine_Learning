d86a4557a66f45fa441a2118ca8dda66533ad0ab
Manh Cuong <cuongpigerr@gmail.com>
2021-05-15 13:21:21 +0700

    .

--- LDS9_K265_DuongManhCuong_Cuoi_ky/bai_tap_1/modules/utils.py
+++ LDS9_K265_DuongManhCuong_Cuoi_ky/bai_tap_1/modules/utils.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import pyspark
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import array, col, explode, lit
 
 
 def createSparkDfFromXlsx(pDf: pd.DataFrame, pSpark: SparkSession):
@@ -9,3 +11,12 @@ def createSparkDfFromXlsx(pDf: pd.DataFrame, pSpark: SparkSession):
     
     return rdd.toDF(columns)
     
+def oversampling(pDf: pyspark.sql.DataFrame, pColumn: str, pMajorValue, pMinorValue):
+    major_df = pDf.filter(col(pColumn) == pMajorValue)
+    minor_df = pDf.filter(col(pColumn) == pMinorValue)
+    ratio = major_df.count() // minor_df.count()
+    
+    oversampled_df = minor_df.withColumn('dummy', explode(array([lit(x) for x in range(ratio)]))).drop('dummy')
+    combined_df = major_df.unionAll(oversampled_df)
+    
+    return combined_df
